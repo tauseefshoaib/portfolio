102f31775259b2e0b6d168ce8273d5c8a86b129f
Tauseef Shoaib <tauseef.shoaib@zenegy.dev>
2026-02-02 14:40:29 +0530

    update and clean up styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ export default function Home() {
     <main className="flex-1 flex items-center pt-20">
       <div className="max-w-5xl mx-auto px-6">
         <h1
-          className="text-5xl md:text-6xl font-bold opacity-0 animate-fade-up"
+          className="text-5xl md:text-6xl font-bold opacity-0 animate-fadeUp"
           style={{ animationDelay: "0.1s" }}
         >
           Hi, I’m <span className="text-white/90">Tauseef Shoaib</span>
         </h1>
 
         <p
-          className="mt-6 text-neutral-400 max-w-xl opacity-0 animate-fade-up"
+          className="mt-6 text-neutral-400 max-w-xl opacity-0 animate-fadeUp"
           style={{ animationDelay: "0.25s" }}
         >
           Software developer focused on building clean, scalable, high-quality
